e04371302c302fddf003690485857d1e427948df
Piotr Kowalski <piecioshka@gmail.com>
2016-04-25 09:23:35 +0200

    Deploy only 2 directories: demo, dist

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -4,9 +4,17 @@ var ghpages = require('gh-pages');
 var path = require('path');
 var root = path.join(__dirname, '..');
 
-ghpages.publish(root, function (err) {
-    if (err) {
-        console.error(err);
+var options = {
+    message: 'Auto-generated commit',
+    src: [
+        'demo/**',
+        'dist/**'
+    ]
+};
+
+ghpages.publish(root, options, function (error) {
+    if (error) {
+        console.error(error);
         return;
     }
 
