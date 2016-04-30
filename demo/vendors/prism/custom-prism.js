bb034da0f25d05ca2885ddcdffcfe106bfe08966
Piotr Kowalski <piecioshka@gmail.com>
2016-04-30 11:43:28 +0200

    Put embed code into demo

---
+++ demo/vendors/prism/custom-prism.js
@@ -0,0 +1,11 @@
+Prism.plugins.NormalizeWhitespace.setDefaults({
+    'remove-trailing': true,
+    'remove-indent': true,
+    'left-trim': true,
+    'right-trim': true,
+    'break-lines': 80,
+    'indent': 0,
+    'remove-initial-line-feed': true,
+    'tabs-to-spaces': 4,
+    'spaces-to-tabs': 4
+});
